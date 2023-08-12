34365b749b4abd9437edeef5ae6da018956ee768
sachin_dashzer <sachin8287037611@gmail.com>
2023-08-13 00:30:32 +0530

    html and css code complete js left

--- main.js
+++ main.js
@@ -1,2 +1,55 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('main'),
+    smooth: true
+});
 
-console.log("this webpage belongs to Dashzer");
+
+function circlefollow(){
+
+    window.addEventListener('mousemove' , function(dets){
+        document.querySelector('#circle').style.transform = `translate(${dets.clientX}px,  ${dets.clientY}px)`;
+    })
+
+}
+
+
+
+circlefollow();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function displaytime(){
+
+    let datetime = new Date();
+    let ghanta = datetime.getHours();
+    let munute = datetime.getMinutes();
+    var session = "AM";
+
+    if(ghanta >= 12){
+        session = "PM"
+        ghanta = ghanta%12;
+    }
+    
+
+
+    document.querySelector('#hrs').innerHTML = ghanta;
+    document.querySelector('#mints').innerHTML = munute;
+    document.querySelector('#session').innerHTML = session;
+}
+
+setInterval(displaytime , 10);
